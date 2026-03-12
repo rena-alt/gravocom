144ff486339e5e764f9e4d02cd2818b55fc82325
Karla Lorena <viajerosdelaluna@gmail.com>
2026-03-12 13:33:19 -0600

    fix: apache logging shown on live

--- netlify/edge-functions/auth.ts
+++ netlify/edge-functions/auth.ts
@@ -4,19 +4,25 @@ import type { Context } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
   const auth = request.headers.get("Authorization");
   
-  // Definimos las credenciales (admin:gravocom2026)
   const user = "admin";
   const pass = "gravocom2026";
-  const expectedAuth = `Basic ${btoa(`${user}:${pass}`)}`;
+  
+  const basicAuth = btoa(`${user}:${pass}`);
+  const expectedAuth = `Basic ${basicAuth}`;
 
   if (auth !== expectedAuth) {
     return new Response("Acceso Restringido - Gravocom Pruebas", {
       status: 401,
       headers: {
         "WWW-Authenticate": 'Basic realm="Acceso restringido"',
+        "Content-Type": "text/plain; charset=utf-8",
       },
     });
   }
 
-  return context.next();
+  return await context.next();
+};
+
+export const config = {
+  path: "/*", 
 };
